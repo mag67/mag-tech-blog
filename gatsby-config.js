ed15d29b254cfa945b41426c905a6e52f7afc2ec
yusaku.komuro <komuroyuusaku@magudon.local>
2021-08-26 00:02:38 +0900

    reset tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,27 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "mag-tech-blog",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'mag-tech-blog',
   },
   plugins: [
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
         name: `blog`,
         path: `${__dirname}/blog`,
       },
     },
-    "gatsby-plugin-mdx",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-mdx',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-plugin-typegen",
+      resolve: 'gatsby-plugin-typegen',
       options: {
         outputPath: `src/__generated__/gatsby-types.d.ts`,
       },
     },
   ],
-  pathPrefix: "/mag-tech-blog",
+  pathPrefix: '/mag-tech-blog',
 };
